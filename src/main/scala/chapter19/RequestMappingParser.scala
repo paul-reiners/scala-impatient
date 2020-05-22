cb337df6aa695b14292423bcaebba5ea3d2ce298
Paul Reiners <paul.reiners@gmail.com>
2020-05-22 17:16:22 -0500

    Created parseAll method.

--- src/main/scala/chapter19/RequestMappingParser.scala
+++ src/main/scala/chapter19/RequestMappingParser.scala
@@ -6,15 +6,20 @@ class RequestMappingParser extends RegexParsers {
   def requestMapping: Parser[Any] = "^@RequestMapping\\(".r ~ commaDelimitedSeq ~ "\\)$".r
   def commaDelimitedSeq: Parser[Any] = keyValue ~ rep("," ~ keyValue)
   def keyValue: Parser[Any] = key ~ "=" ~ value
-  def key: Parser[Any] = """[^)=]*""".r
+  def key: Parser[Any] = "value" | "method"
   def value: Parser[Any] = """[^)]*""".r
 }
 
 object RequestMappingParser {
   def main(args: Array[String]): Unit = {
+    val result = parseAll
+    if (result.successful) println(result.get)
+  }
+
+  private def parseAll = {
     val parser = new RequestMappingParser
     val result =
       parser.parseAll(parser.requestMapping, """@RequestMapping(value = "/ex/foos", method = RequestMethod.GET)""")
-    if (result.successful) println(result.get)
+    result
   }
 }
